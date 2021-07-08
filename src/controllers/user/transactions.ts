b3be1f708174c14959b90bd9d48d1f3f05fbd283
ShubhamPalriwala <spalriwalau@gmail.com>
2021-07-08 10:14:15 +0530

    feat: in-event transaction

--- src/controllers/user/transactions.ts
+++ src/controllers/user/transactions.ts
@@ -4,10 +4,9 @@ import Admin, { adminModel } from "../../db/models/Admin";
 import Budget, { budgetModel } from "../../db/models/Budget";
 import Offer, { offerModel } from "../../db/models/Offer";
 import { eventModel } from "../../db/models/Event";
-import Transaction, { transactionModel } from "../../db/models/Transaction";
-import UserEventMapping, {
-  userEventMappingModel,
-} from "../../db/models/UserEventMapping";
+import { transactionModel } from "../../db/models/Transaction";
+import { userEventMappingModel } from "../../db/models/UserEventMapping";
+import { InternalErrorResponse, SuccessResponse } from "../../core/ApiResponse";
 
 class transactionController {
   payForEvent = async (req: Request, res: Response): Promise<void> => {
@@ -42,7 +41,6 @@ class transactionController {
         price: number;
         description: string;
       };
-      console.log(eventToReducePassQuantityFound);
 
       eventToReducePassQuantityFound.passes.forEach(
         (pass: {
@@ -52,7 +50,6 @@ class transactionController {
           price: number;
           description: string;
         }) => {
-          // console.log(pass);
           if (pass.id === passId) {
             userPass = pass;
             pass.availableNumber -= 1;
@@ -68,12 +65,9 @@ class transactionController {
           new: true,
         })
         .session(session);
-      console.log(updatedEvent);
 
       if (offerId) {
         const offerDetails = await offerModel.findById(offerId);
-        console.log(offerDetails.scope.eventId);
-        console.log(offerDetails.scope.userId);
 
         if (
           offerDetails.scope.eventId.includes(eventId) ||
@@ -96,7 +90,6 @@ class transactionController {
         const updatedBudget = await budgetModel
           .findOneAndUpdate({ userId, eventId }, budgetFound, { new: true })
           .session(session);
-        console.log(updatedBudget);
         userBudget = updatedBudget;
       }
 
@@ -122,94 +115,222 @@ class transactionController {
           { session }
         );
         newUserInEvent = await userEventMappingModel.create(
-          {
-            eventId,
-            userId,
-            transactions: entryInEventTransaction.id,
-            budget: userBudget.id,
-            passType: userPass.id,
-          },
+          [
+            {
+              eventId,
+              userId,
+              transactions: [entryInEventTransaction[0].id],
+              budget: userBudget.id,
+              passType: userPass.id,
+            },
+          ],
           { session }
         );
       } else if (userBudget && !offerApplicable) {
         entryInEventTransaction = await transactionModel.create(
-          {
-            senderId: userId,
-            receiverId: adminOfEvent.id,
-            eventId,
-            amount: userPass.price,
-            type: "event-entry",
-            transactionStatus: "success",
-            rapydId,
-          },
+          [
+            {
+              senderId: userId,
+              receiverId: adminOfEvent.id,
+              eventId,
+              amount: userPass.price,
+              type: "event-entry",
+              transactionStatus: "success",
+              rapydId,
+            },
+          ],
           { session }
         );
         newUserInEvent = await userEventMappingModel.create(
-          {
-            eventId,
-            userId,
-            transactions: entryInEventTransaction.id,
-            budget: userBudget.id,
-            passType: userPass.id,
-          },
+          [
+            {
+              eventId,
+              userId,
+              transactions: [entryInEventTransaction[0].id],
+              budget: userBudget.id,
+              passType: userPass.id,
+            },
+          ],
           { session }
         );
       } else if (!userBudget && offerApplicable) {
         entryInEventTransaction = await transactionModel.create(
-          {
-            senderId: userId,
-            receiverId: adminOfEvent.id,
-            eventId,
-            amount: userPass.price - offerApplicable.value,
-            offerId: offerApplicable.id,
-            type: "event-entry",
-            transactionStatus: "success",
-            rapydId,
-          },
+          [
+            {
+              senderId: userId,
+              receiverId: adminOfEvent.id,
+              eventId,
+              amount: userPass.price - offerApplicable.value,
+              offerId: offerApplicable.id,
+              type: "event-entry",
+              transactionStatus: "success",
+              rapydId,
+            },
+          ],
           { session }
         );
         newUserInEvent = await userEventMappingModel.create(
-          {
-            eventId,
-            userId,
-            transactions: entryInEventTransaction.id,
-            passType: userPass.id,
-          },
+          [
+            {
+              eventId,
+              userId,
+              transactions: [entryInEventTransaction[0].id],
+              passType: userPass.id,
+            },
+          ],
           { session }
         );
       } else if (!userBudget && !offerApplicable) {
         entryInEventTransaction = await transactionModel.create(
-          {
-            senderId: userId,
-            receiverId: adminOfEvent.id,
-            eventId,
-            amount: userPass.price,
-            type: "event-entry",
-            transactionStatus: "success",
-            rapydId,
-          },
+          [
+            {
+              senderId: userId,
+              receiverId: adminOfEvent.id,
+              eventId,
+              amount: userPass.price,
+              type: "event-entry",
+              transactionStatus: "success",
+              rapydId,
+            },
+          ],
           { session }
         );
         newUserInEvent = await userEventMappingModel.create(
-          {
-            eventId,
-            userId,
-            transactions: entryInEventTransaction.id,
-            passType: userPass.id,
-          },
+          [
+            {
+              eventId,
+              userId,
+              transactions: [entryInEventTransaction[0].id],
+              passType: userPass.id,
+            },
+          ],
           { session }
         );
       }
       await session.commitTransaction();
 
-      res.send({
+      new SuccessResponse("Welcome to the event", {
         entryInEventTransaction,
         newUserInEvent,
-      });
+      }).send(res);
     } catch (err) {
       await session.abortTransaction();
+      new InternalErrorResponse(err.message, {}).send(res);
 
       console.log(err);
+    } finally {
+      session.endSession();
+    }
+  };
+
+  payInEvent = async (req: Request, res: Response): Promise<void> => {
+    const session: ClientSession = await startSession();
+    session.startTransaction();
+
+    try {
+      const userId = req.user.id;
+      const { eventId, rapydId, offerId, amount, receiverId } = req.body;
+      let offerApplicable: Offer;
+      let userBudget: Budget;
+      let entryInEventTransaction;
+
+      if (amount <= 0 || !receiverId) {
+        throw new Error("Invalid Request, Please try again");
+      }
+
+      const userAlreadyInEvent = await userEventMappingModel.findOne({
+        userId,
+        eventId,
+      });
+      if (!userAlreadyInEvent) {
+        throw new Error(
+          "User has not joined the event yet. Please buy pass first"
+        );
+      }
+
+      if (offerId) {
+        const offerDetails = await offerModel.findById(offerId);
+        if (
+          offerDetails.scope.eventId.includes(eventId) ||
+          offerDetails.scope.userId.includes(userId)
+        ) {
+          offerApplicable = offerDetails;
+        } else {
+          throw new Error("Offer cannot be used in this event by you");
+        }
+      }
+
+      const budgetFound = await budgetModel.findOne({ userId, eventId });
+      if (budgetFound) {
+        if (offerApplicable) {
+          budgetFound.amountSpent += amount - offerApplicable.value;
+        } else {
+          budgetFound.amountSpent += amount;
+        }
+
+        const updatedBudget = await budgetModel
+          .findOneAndUpdate({ userId, eventId }, budgetFound, { new: true })
+          .session(session);
+        userBudget = updatedBudget;
+      }
+
+      const adminOfEvent: Admin = await adminModel.findOne({ eventId });
+      if (!adminOfEvent) {
+        throw new Error("No such event exists");
+      }
+
+      if (offerApplicable) {
+        entryInEventTransaction = await transactionModel.create(
+          [
+            {
+              senderId: userId,
+              receiverId,
+              eventId,
+              amount: amount - offerApplicable.value,
+              offerId: offerApplicable.id,
+              type: "in-event",
+              transactionStatus: "success",
+              rapydId,
+            },
+          ],
+
+          { session }
+        );
+      } else {
+        entryInEventTransaction = await transactionModel.create(
+          [
+            {
+              senderId: userId,
+              receiverId,
+              eventId,
+              amount,
+              type: "in-event",
+              transactionStatus: "success",
+              rapydId,
+            },
+          ],
+          { session }
+        );
+      }
+      const AddTransactionInMapping =
+        await userEventMappingModel.findOneAndUpdate(
+          { eventId, userId },
+          {
+            $push: { transactions: entryInEventTransaction[0].id },
+          },
+          { new: true, session }
+        );
+
+      await session.commitTransaction();
+
+      new SuccessResponse("Succesfully paid for the in-event transaction", {
+        entryInEventTransaction,
+        AddTransactionInMapping,
+      }).send(res);
+    } catch (err) {
+      await session.abortTransaction();
+      console.log(err);
+      new InternalErrorResponse(err.message, {}).send(res);
     } finally {
       session.endSession();
     }
--- src/routes/user/transaction.ts
+++ src/routes/user/transaction.ts
@@ -6,5 +6,6 @@ const transactionRouter = Router();
 const transactionController = new TransactionController();
 
 transactionRouter.post("/pay-for-event", transactionController.payForEvent);
+transactionRouter.post("/pay-in-event", transactionController.payInEvent);
 
 export default transactionRouter;
